a92cfa9ecd691bb25152b3e7aa3ced760c872595
Sergey Smetannikov <sergenevsky@gmail.com>
2025-01-29 11:42:20 +0700

    README.md update, fixed spacing in TodoItem.tsx

--- README.md
+++ README.md
@@ -7,3 +7,8 @@ Self-education project in order to learn React and it's components, including us
 - Save & Load picked "Label" to/from localStorage
 - Additional styling
 - Feature to build "app" using picked "Label"
+
+## Run locally
+
+- Setup yarn, cd into todo-app catalog using `cd todo-app` command and execute `yarn` or `yarn install` command, in onder to install necessary dependecies.
+- Then run the app using `yarn start` command & follow the instructions in the terminal.
--- todo-app/src/components/TodoItem.tsx
+++ todo-app/src/components/TodoItem.tsx
@@ -21,14 +21,18 @@ export const TodoItem: React.FC<TodoItemProps> = ({
   onDelete,
 }) => {
   return (
-    <li className="flex flex-row border-t mt-2 pt-2">
+    <li className="flex flex-row border-t mt-2 pt-2 justify-between">
       <div
-        className={`mr-2 max-w-[193px] line-clamp-3 ${todo.completed ? "line-through opacity-50" : ""}`}
+        className={`mr-2 min-w-[193px] max-w-[193px] line-clamp-3 ${todo.completed ? "line-through opacity-50" : ""}`}
       >
         {todo.text}
       </div>
-      <Button title="Toggle" func={() => onToggle(todo.id)} />
-      <Button title="Delete" func={() => onDelete(todo.id)} />
+      <Button title="Done" func={() => onToggle(todo.id)} />
+      <Button
+        title="Delete"
+        color="bg-red-400"
+        func={() => onDelete(todo.id)}
+      />
     </li>
   );
 };
